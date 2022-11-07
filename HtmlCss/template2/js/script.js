9dc1220ff11708d643aed6267962a18916b92635
Koengreen <302261150@student.rocmondriaan.nl>
2022-11-07 12:19:44 +0100

    info page gemaakt

--- HtmlCss/template2/js/script.js
+++ HtmlCss/template2/js/script.js
@@ -12,3 +12,21 @@ function carousel() {
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 2000);
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("carrousel");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
